05d2bee33c116a7149407efd8969b78d5bb601d8
Aditya Sebastian <aditya26824@gmail.com>
2025-07-24 22:53:52 +0530

    Added Q12

---
+++ Q12/personal_information_collector.py
@@ -0,0 +1,6 @@
+Name = input("Enter your full name : ")
+Age = int(input("Enter your age : "))
+City = input("Enter your city : ")
+Hobby = input("Enter your Hobby : ")
+
+print(f"Hello, {Name}!\nYou are {Age} years old and live in {City}.\nIn your free time, you enjoy {Hobby}.")
